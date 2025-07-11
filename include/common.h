82eec3ef4520c335590d30441faf4bcbc4ce4a77
zc <wzzc@wzzc.dev>
2025-07-11 18:47:18 +0800

    Implement initial project structure with build configuration and core functionality

---
+++ include/common.h
@@ -0,0 +1,8 @@
+#ifndef ju_common_h
+#define ju_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
---
+++ include/debug.h
@@ -0,0 +1,9 @@
+#ifndef ju_debug_h
+#define ju_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
